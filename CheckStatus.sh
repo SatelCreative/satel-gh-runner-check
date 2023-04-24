cc790e306668c7da8e9342102a22a74962648f92
Rahul <rahulpatidar0191@gmail.com>
2023-04-24 12:05:04 -0700

    debug echo

--- CheckStatus.sh
+++ CheckStatus.sh
@@ -6,7 +6,7 @@ STATUS=$(curl -L \
         -H "X-GitHub-Api-Version: ${GITHUB_API_VERSION}" \
         https://api.github.com/orgs/SatelCreative/actions/runners | jq '.runners[] | select(.name == "${RUNNER_NAME}") | .status')
 
-echo ${STATUS}
+echo "Print ${STATUS}"
 
 # These outputs are used in other steps/jobs via action.yml
 echo "::set-output name=preview_link::${STATUS}" 
