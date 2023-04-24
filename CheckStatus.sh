15fc77f6954dafdbbacec584dbfc5bfee340ee0b
Rahul <rahulpatidar0191@gmail.com>
2023-04-24 10:55:44 -0700

    remove echo

--- CheckStatus.sh
+++ CheckStatus.sh
@@ -6,5 +6,3 @@ STATUS=$(curl -L \
         -H "X-GitHub-Api-Version: "${{ GITHUB_API_VERSION }}" \
         https://api.github.com/orgs/SatelCreative/actions/runners | jq '.runners[] | select(.name == "${{ RUNNER_NAME }}") | .status')
 echo $STATUS  
-
-# echo "::set-output name=status::${STATUS}" 
