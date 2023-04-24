9c0ebdedbbc389a2d4abe5639e8b3bafca258130
Rahul <rahulpatidar0191@gmail.com>
2023-04-24 11:04:57 -0700

    try double quotes

--- CheckStatus.sh
+++ CheckStatus.sh
@@ -4,6 +4,6 @@ STATUS=`curl -L \
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer ${GITHUB_ADMIN_TOKEN}"\
         -H "X-GitHub-Api-Version: "${GITHUB_API_VERSION}" \
-        https://api.github.com/orgs/SatelCreative/actions/runners | jq '.runners[] | select(.name == "cosmicray") | .status'`
+        https://api.github.com/orgs/SatelCreative/actions/runners | jq ".runners[] | select(.name == 'cosmicray') | .status"`
 
 echo $STATUS  
