54e0044dff9f72036058268e4f4e834ad71a7d51
Rahul <rahulpatidar0191@gmail.com>
2023-04-24 12:29:03 -0700

    change variable

--- CheckStatus.sh
+++ CheckStatus.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-STATUS=`curl -L \
+STATUS=$(curl -L \
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer ${GITHUB_ADMIN_TOKEN}" \
         -H "X-GitHub-Api-Version: ${GITHUB_API_VERSION}" \
-        https://api.github.com/orgs/SatelCreative/actions/runners | jq '.runners[] | select(.name == '\"${RUNNER_NAME}\"') | .status'`
+        https://api.github.com/orgs/SatelCreative/actions/runners | jq '.runners[] | select(.name == '\"${RUNNER_NAME}\"') | .status')
 
 echo "Print ${STATUS}"
 
