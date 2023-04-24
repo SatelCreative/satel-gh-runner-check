39ab112b37adef8917123669de4deea27691d3e6
Rahul <rahulpatidar0191@gmail.com>
2023-04-24 11:00:55 -0700

    fix sytax

--- CheckStatus.sh
+++ CheckStatus.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-STATUS=$(curl -L \
+STATUS=`curl -L \
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer ${{ GITHUB_ADMIN_TOKEN }}"\
         -H "X-GitHub-Api-Version: "${{ GITHUB_API_VERSION }}" \
-        https://api.github.com/orgs/SatelCreative/actions/runners | jq '.runners[] | select(.name == "cosmicray") | .status')
+        https://api.github.com/orgs/SatelCreative/actions/runners | jq '.runners[] | select(.name == "cosmicray") | .status'`
+
 echo $STATUS  
