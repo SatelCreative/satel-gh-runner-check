ef1edd8674c861f7ce55803c21e1b347e912d706
Rahul <rahulpatidar0191@gmail.com>
2023-04-24 11:59:07 -0700

    enable o/p

--- CheckStatus.sh
+++ CheckStatus.sh
@@ -4,7 +4,10 @@ STATUS=$(curl -L \
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer ${GITHUB_ADMIN_TOKEN}" \
         -H "X-GitHub-Api-Version: ${GITHUB_API_VERSION}" \
-        https://api.github.com/orgs/SatelCreative/actions/runners | jq '.runners[] | select(.name == "cosmicray") | .status')
+        https://api.github.com/orgs/SatelCreative/actions/runners | jq '.runners[] | select(.name == "${RUNNER_NAME}") | .status')
 
 echo "$STATUS"
+
+# These outputs are used in other steps/jobs via action.yml
+echo "::set-output name=preview_link::${STATUS}" 
 
