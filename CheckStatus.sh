70df9e6a863f33acd50866e971fcaf6794753f82
Rahul <rahulpatidar0191@gmail.com>
2023-04-24 10:51:22 -0700

    turn o/p off for now

--- CheckStatus.sh
+++ CheckStatus.sh
@@ -7,4 +7,4 @@ STATUS=$(curl -L \
         https://api.github.com/orgs/SatelCreative/actions/runners | jq '.runners[] | select(.name == "${{ RUNNER_NAME }}") | .status')
 echo $STATUS  
 
-echo "::set-output name=status::${STATUS}" 
+# echo "::set-output name=status::${STATUS}" 
