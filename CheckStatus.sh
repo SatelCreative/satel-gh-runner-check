b5e1573e78379d67ddd15cca25612a34b09e4368
Rahul <rahulpatidar0191@gmail.com>
2023-04-24 12:20:23 -0700

    replace double quotes

--- CheckStatus.sh
+++ CheckStatus.sh
@@ -4,7 +4,7 @@ STATUS=`curl -L \
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer ${GITHUB_ADMIN_TOKEN}" \
         -H "X-GitHub-Api-Version: ${GITHUB_API_VERSION}" \
-        https://api.github.com/orgs/SatelCreative/actions/runners | jq ".runners[] | select(.name == \"${RUNNER_NAME}\") | .status"`
+        https://api.github.com/orgs/SatelCreative/actions/runners | jq '.runners[] | select(.name == \"${RUNNER_NAME}\") | .status'`
 
 echo "Print ${STATUS}"
 
