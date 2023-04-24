8c6e764d44de5c39d999836e288ed7a4f2f9fb08
Rahul <rahulpatidar0191@gmail.com>
2023-04-24 13:05:49 -0700

    add array for status

--- CheckStatus.sh
+++ CheckStatus.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
-RUNNER_NAME=$1
+RUNNER_NAMES=$1
+STATUSES=()
 
-RESPONSE=$(curl -L \
+function check_status(){
+    RUNNER_NAME=$1
+    RESPONSE=$(curl -L \
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer ${GITHUB_ADMIN_TOKEN}" \
         -H "X-GitHub-Api-Version: ${GITHUB_API_VERSION}" \
         https://api.github.com/orgs/SatelCreative/actions/runners)
 
-STATUS=$(echo "${RESPONSE}" | jq ".runners[] | select(.name == \"${RUNNER_NAME}\") | .status")
+    STATUS=$(echo "${RESPONSE}" | jq ".runners[] | select(.name == \"${RUNNER_NAME}\") | .status")
+    STATUSES+=(${STATUS})
+    echo "STATUS=${STATUSES}"
 
-echo "STATUS=${STATUS}"
+}
+
+runners=( ${RUNNER_NAMES} )
+for runner in "${runners[@]}"
+do
+deploy_pr_branch_or_tag "${runners}"
+done 
 
 # These outputs are used in other steps/jobs via action.yml
-echo "::set-output name=status::${STATUS}"
+echo "::set-output name=status::${STATUSES[@]}"
