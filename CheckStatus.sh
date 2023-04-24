722911e84e8fcd283ad9c0b0bf954f45b3111f3e
Rahul <rahulpatidar0191@gmail.com>
2023-04-24 12:55:59 -0700

    add o/p

--- CheckStatus.sh
+++ CheckStatus.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 RUNNER_NAME=$1
-echo "RUNNER_NAME=${RUNNER_NAME}"
 
 RESPONSE=$(curl -L \
         -H "Accept: application/vnd.github+json" \
@@ -8,11 +7,9 @@ RESPONSE=$(curl -L \
         -H "X-GitHub-Api-Version: ${GITHUB_API_VERSION}" \
         https://api.github.com/orgs/SatelCreative/actions/runners)
 
-#RUNNER_NAME="cosmicray"
-
 STATUS=$(echo "${RESPONSE}" | jq ".runners[] | select(.name == \"${RUNNER_NAME}\") | .status")
 
 echo "STATUS=${STATUS}"
 
 # These outputs are used in other steps/jobs via action.yml
-echo "::set-output name=preview_link::${STATUS}"
+echo "::set-output name=status::${STATUS}"
