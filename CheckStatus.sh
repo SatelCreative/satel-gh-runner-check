c42ac48289e7e760e3c14e781d99bf5d42f3e3bf
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2023-06-01 14:59:07 -0700

    print runner

--- CheckStatus.sh
+++ CheckStatus.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 STATUSES=()
-ERRORS=()
 
-function check_status() {
+function check_status(){
     RUNNER_NAME=$1
     RESPONSE=$(curl -L \
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer ${GITHUB_ADMIN_TOKEN}" \
         -H "X-GitHub-Api-Version: ${GITHUB_API_VERSION}" \
-        "https://api.github.com/orgs/${ORG_NAME}/actions/runners")
+        https://api.github.com/orgs/${ORG_NAME}/actions/runners)
         
-    echo "RESPONSE=${RESPONSE}"
-    STATUS=$(echo "${RESPONSE}" | jq -r --arg runnerName "${RUNNER_NAME}" \
-    '.runners | map(select(.name == $runnerName)) | .[0].status || "UNKNOWN"' 2>&1)
-    # STATUS=$(echo "${RESPONSE}" | jq ".runners[] | select(.name == \"${RUNNER_NAME}\") | .status" 2>&1)
-    echo "STATUS=${STATUS}"
-    if [ $? -ne 0 ]; then
-        ERRORS+=("${STATUS}")
-    else
-        STATUSES+=("${STATUS}")
+    RUNNERS=$(echo "${RESPONSE}" | jq -r '.runners')
+    if [ "${RUNNERS}" = "null" ]; then
+        ERRORS+=("No runners found in the response.")
+        return
     fi
+    
+    STATUS=$(echo "${RESPONSE}" | jq ".runners[] | select(.name == \"${RUNNER_NAME}\") | .status")
+    STATUSES+=(${STATUS})
 }
 
-runners=(${RUNNER_NAMES})
-for runner in "${runners[@]}"; do
-    check_status "${runner}"
-done
+runners=( ${RUNNER_NAMES} )
+for runner in "${runners[@]}"
+do
+check_status "${runner}"
+done 
 
 # These outputs are used in other steps/jobs via action.yml
 echo "::set-output name=status::${STATUSES[@]}"
-echo "::set-output name=errors::${ERRORS[@]}"
