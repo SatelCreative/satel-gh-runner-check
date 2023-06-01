8d74a0602839a269dbee7dbaee4c88f1708c5a94
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2023-06-01 14:12:13 -0700

    add exit codes

--- CheckStatus.sh
+++ CheckStatus.sh
@@ -7,7 +7,12 @@ function check_status(){
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer ${GITHUB_ADMIN_TOKEN}" \
         -H "X-GitHub-Api-Version: ${GITHUB_API_VERSION}" \
-        https://api.github.com/orgs/${ORG_NAME}/actions/runners)
+        https://api.github.com/orgs/${ORG_NAME}/actions/runners; STATUS1=$?)
+    
+    if [[ $STATUS1 == 0 ]]
+    then 
+        exit $STATUS1  
+    fi   
 
     STATUS=$(echo "${RESPONSE}" | jq ".runners[] | select(.name == \"${RUNNER_NAME}\") | .status")
     STATUSES+=(${STATUS})
