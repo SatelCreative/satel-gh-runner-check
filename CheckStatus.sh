ec5b92ba9af559781ac09298127fece74db17b64
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2023-06-12 14:53:18 -0700

    ✨ Catch exit code for expired token  (#3)
    * add exit codes
    
    * add exit codes
    
    * print status
    
    * debug
    
    * print unknown
    
    * fix or
    
    * print runner
    
    * print runner again
    
    * add exit code
    
    * remove debug
    
    * keep on runner
    
    * add debug
    
    * add both runners
    
    * remove debugs
    
    * add text
    
    * add new details
    
    * fix token name

--- CheckStatus.sh
+++ CheckStatus.sh
@@ -5,9 +5,16 @@ function check_status(){
     RUNNER_NAME=$1
     RESPONSE=$(curl -L \
         -H "Accept: application/vnd.github+json" \
-        -H "Authorization: Bearer ${GITHUB_ADMIN_TOKEN}" \
+        -H "Authorization: Bearer ${GITHUB_RUNNER_TOKEN}" \
         -H "X-GitHub-Api-Version: ${GITHUB_API_VERSION}" \
         https://api.github.com/orgs/${ORG_NAME}/actions/runners)
+ 
+    RUNNERS=$(echo "${RESPONSE}" | jq -r '.runners')
+    if [ "${RUNNERS}" = "null" ]; then
+        echo "Problem with the token"
+        exit 1
+        return
+    fi
 
     STATUS=$(echo "${RESPONSE}" | jq ".runners[] | select(.name == \"${RUNNER_NAME}\") | .status")
     STATUSES+=(${STATUS})
